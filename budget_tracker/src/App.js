f4667842e28c3e4941695b2883907664289aebd0
saberin19 <valeriemys19@gmail.com>
2021-11-11 12:17:26 +0800

    login page

--- budget_tracker/src/App.js
+++ budget_tracker/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './containers/Login';
+import LoginPage from './pages/login-page'
 
 function App() {
   return (
     <div className="App">
-     <Login />
+     <LoginPage />
     </div>
   );
 }
---
+++ budget_tracker/src/pages/login-page.js
@@ -0,0 +1,11 @@
+import Login from "../containers/Login";
+
+function LoginPage() {
+    return (
+        <div className="login-page">
+             <Login/>
+        </div>
+    );
+};
+
+export default LoginPage;
