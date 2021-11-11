95138fc11d6575280cccea5b19a67b8cd1b9a911
Wai Jie <cwj94321@gmail.com>
2021-11-11 15:55:39 +0800

    help me test this

--- WJ/app.py
+++ WJ/app.py
@@ -5,7 +5,7 @@ from . import db
 import json 
 from sqlalchemy.sql import func
 
-# - View all projects under certain user 
+# View all projects under that user 
 
 projects = Blueprint('projects', __name__)
 
@@ -18,14 +18,11 @@ def projects_serializer(projects):
         'expenses':projects.expenses
         }
 
-@app.route("/view_projects/<int:user_id>)
+@jwt_required()
+@app.route("/view_projects/<int:user_id>")
 def all_projects(user_id):
     projects= Project.query.filter_by(user_id=user_id).all()
     if projects:
         return jsonify([*map(projects_serializer, projects)])
     else:
         return jsonify({'404': 'No projects found'})
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
