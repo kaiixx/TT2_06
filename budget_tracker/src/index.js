f9beef2f59699c87a2f74a2e5051ed41928a51ad
Valerie Ma <91739746+valmys@users.noreply.github.com>
2021-11-11 12:42:05 +0700

    update router for index.js

--- budget_tracker/src/index.js
+++ budget_tracker/src/index.js
@@ -7,7 +7,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
